9a641e1d03915f4049b0eb638ca8e1a6a8865faf
ishaqniazi <160505998+ishaqniazi@users.noreply.github.com>
2025-02-25 03:23:38 +0500

    day1 file uploaded

---
+++ Day1.py
@@ -0,0 +1,5 @@
+First_Name = "Muhammad"
+Last_Name = "Ishaq Niazi"
+Print = "Hello, Python Learners Today is the first step in our coding journey"
+print(Print)
+print(f"{First_Name} {Last_Name} here, {Print}")
